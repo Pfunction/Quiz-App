7505179522a5186a6f058bde3f3b7accb7aa3dc6
Pfunction <pauliuspundz@gmail.com>
2025-01-23 18:13:08 +0200

    *Initial back-end integration*--
    Setting Up the Development Environment:
    Installed .NET SDK.
    Added EF Core In-Memory Database package.
    Created Models (Quiz, Question, Answer, HighScore, QuizSubmission, SubmissionAnswer).
    Created QuizContext for EF Core.
    Configured the In-Memory Database in Program.cs.
    
    Building the API Endpoints:
    Created QuizzesController with endpoints for creating quizzes, retrieving quizzes, submitting quiz answers, and retrieving high scores.

---
+++ QuizApp/Models/Answer.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace QuizApp.Models
+{
+    public class Answer
+    {
+        public int Id {get;set;}
+        public string Text {get;set;}
+        public bool IsCorrect {get; set;}
+    }
+}
