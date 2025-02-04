d815a9b220cdc354bc299d0bd6b61367005b8821
Pfunction <pauliuspundz@gmail.com>
2025-02-04 09:00:51 +0200

    assemblyinfochange

--- obj/Debug/net9.0/QuizApp.AssemblyInfo.cs
+++ obj/Debug/net9.0/QuizApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("QuizApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b5dca3cf1a4dea10f60c1ce41278f0a1a4ead61d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+61cecaafe683f7570ebf18488e910f225eacc8be")]
 [assembly: System.Reflection.AssemblyProductAttribute("QuizApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("QuizApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
